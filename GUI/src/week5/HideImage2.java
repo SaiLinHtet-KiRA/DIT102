8db402e986750050485c340df17f5869c240e8e9
KiRA <sailinhtet.d@gmail.com>
2026-02-17 22:50:59 -0800

    week6

--- GUI/src/week5/HideImage2.java
+++ GUI/src/week5/HideImage2.java
@@ -3,17 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package week5;
-
+import java.util.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Sai Lin Htet
  */
-public class HideImage extends javax.swing.JFrame {
+public class HideImage2 extends javax.swing.JFrame {
 
     /**
      * Creates new form HideImage
      */
-    public HideImage() {
+    public HideImage2() {
         initComponents();
     }
 
@@ -26,59 +27,43 @@ public class HideImage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnClick = new javax.swing.JButton();
         lblShow = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        btnClick.setText("Hide");
-        btnClick.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnClickActionPerformed(evt);
-            }
-        });
-
         lblShow.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imgs/cat.jpg"))); // NOI18N
         lblShow.setText("jLabel1");
+        lblShow.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblShowMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(112, 112, 112)
-                        .addComponent(lblShow, javax.swing.GroupLayout.PREFERRED_SIZE, 386, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(255, 255, 255)
-                        .addComponent(btnClick)))
-                .addContainerGap(377, Short.MAX_VALUE))
+                .addGap(97, 97, 97)
+                .addComponent(lblShow, javax.swing.GroupLayout.PREFERRED_SIZE, 464, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(314, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(57, 57, 57)
-                .addComponent(btnClick)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(53, 53, 53)
                 .addComponent(lblShow, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(263, Short.MAX_VALUE))
+                .addContainerGap(302, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnClickActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClickActionPerformed
+    private void lblShowMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblShowMouseClicked
         // TODO add your handling code here:
-        if(btnClick.getText().equals("Hide")){
-                    lblShow.setVisible(false);
-btnClick.setText("Show");
-        }else{
-                lblShow.setVisible(true);
-btnClick.setText("Hide");
-
-    }
-    }//GEN-LAST:event_btnClickActionPerformed
+        if(evt.getButton() == java.awt.event.MouseEvent.BUTTON1) JOptionPane.showMessageDialog(rootPane, "Mouse Clicked");
+        else if(evt.getButton() == java.awt.event.MouseEvent.BUTTON3) JOptionPane.showMessageDialog(rootPane, "dont do this");
+    }//GEN-LAST:event_lblShowMouseClicked
 
     /**
      * @param args the command line arguments
@@ -97,26 +82,26 @@ btnClick.setText("Hide");
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HideImage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HideImage2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HideImage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HideImage2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HideImage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HideImage2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HideImage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HideImage2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new HideImage().setVisible(true);
+                new HideImage2().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnClick;
     private javax.swing.JLabel lblShow;
     // End of variables declaration//GEN-END:variables
 }
